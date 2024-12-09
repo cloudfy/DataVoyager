fa0339036aa53fc7696ee3138684fd067cd6f925
Rasmus Foged <rasmusf@plude.io>
2024-12-09 14:12:55 +0100

    Update packaging and refactor GenerateSchema method
    Updated DataVoyager.Cli.csproj to include .NET tool packaging properties. Refactored GenerateSchema method in DatabaseExporter.cs to be synchronous and removed a large block of commented-out code.

--- DataVoyager.Core/Export/DatabaseExporter.cs
+++ DataVoyager.Core/Export/DatabaseExporter.cs
@@ -26,7 +26,7 @@ public sealed class DatabaseExporter(
         ReadyBuild(outputDetails);
 
         // generate schema and data
-        await GenerateSchema(connection, outputDetails, cancellationToken);
+        GenerateSchema(connection, outputDetails);
         await ExportData(connection, outputDetails, options.IgnoreObjects, cancellationToken);
 
         await connection.CloseAsync();
@@ -130,10 +130,9 @@ public sealed class DatabaseExporter(
             }
         }
     }
-    private async Task GenerateSchema(
+    private void GenerateSchema(
         SqlConnection sqlConnection
-        , OutputInformation output
-        , CancellationToken cancellationToken = default)
+        , OutputInformation output)
     {
         var sc = new ServerConnection(sqlConnection);
         sc.Connect();
@@ -186,182 +185,5 @@ public sealed class DatabaseExporter(
             , Users = false
             , OutputFilename = outputFile
         });
-
-        //using var outputWriter = new StreamWriter(outputFile);
-
-        //foreach (var db in server.Databases
-        //    .Cast<Database>()
-        //    .AsQueryable()
-        //    .Where(o => o.IsSystemObject == false))
-        //{
-        //    if (db.IsSystemObject)
-        //        continue;
-
-        //    _logger.LogInformation($"Exporting database - {db.Name}");
-
-        //    if (_options.ScriptDatabase)
-        //    {
-        //        /// *************************************************************
-        //        /// Database
-        //        Helpers.WriteSQLInner<Database>(
-        //            db.Name, "", "DB", db.Name, outputWriter, db, ScriptOption.Default, _logger);
-        //    }
-        //    if (_options.ScriptSchema)
-        //    {
-        //        /// *************************************************************
-        //        /// Schema
-        //        foreach (var schema2 in db.Schemas.Cast<Schema>().AsQueryable())
-        //        {
-        //            Helpers.WriteSQLInner<Schema>(
-        //                db.Name, "", "Schema", schema2.Name, outputWriter, schema2, ScriptOption.ScriptSchema, _logger);
-        //        }
-        //    }
-
-        //    if (_options.ScriptUserDefinedTypes) 
-        //    { 
-        //        /// *************************************************************
-        //        /// DB USER TYPES
-        //        // currentPath = csFile.CreateFolder(dbPath, pathify("UTYPE"));
-        //        foreach (UserDefinedType o in db.UserDefinedTypes)
-        //        {
-        //            Helpers.WriteSQLInner<UserDefinedType>(db.Name, o.Schema, "UTYPE", o.Name, outputWriter, o, ScriptOption.Default, _logger);
-        //        }
-        //    }
-
-        //    /// *************************************************************
-        //    /// DB TRIGGERS
-        //    //currentPath = csFile.CreateFolder(dbPath, pathify("TRIGGER"));
-        //    foreach (DatabaseDdlTrigger o in db.Triggers.Cast<DatabaseDdlTrigger>().AsQueryable().Where(o => o.IsSystemObject == false))
-        //    {
-        //        Helpers.WriteSQLInner<DatabaseDdlTrigger>(
-        //            db.Name, "dbo", "TRIGGER", o.Name, outputWriter, o, ScriptOption.Triggers, _logger);
-        //    }
-
-        //    /// *************************************************************
-        //    /// DB USER TABLE TYPES
-        //    //currentPath = csFile.CreateFolder(dbPath, pathify("TTYPES"));
-        //    foreach (UserDefinedTableType o in db.UserDefinedTableTypes)
-        //    {
-        //        Helpers.WriteSQLInner<UserDefinedTableType>(db.Name, o.Schema, "TTYPES", o.Name, outputWriter, o, ScriptOption.Default, _logger);
-        //    }
-
-        //    /// *************************************************************
-        //    /// DB FULLTEXT CATALOGS
-        //    if (_options.ExportFullText)
-        //    {
-        //        //currentPath = csFile.CreateFolder(dbPath, pathify("FTC"));
-        //        foreach (FullTextCatalog o in db.FullTextCatalogs)
-        //        {
-        //            Helpers.WriteSQLInner<FullTextCatalog>(
-        //                db.Name, "dbo", "FTC", o.Name, outputWriter, o, ScriptOption.FullTextCatalogs, _logger);
-        //        }
-
-        //        /// *************************************************************
-        //        /// DB FULLTEXT STOPLISTS
-        //        //currentPath = csFile.CreateFolder(dbPath, pathify("FTL"));
-        //        foreach (FullTextStopList o in db.FullTextStopLists)
-        //        {
-        //            // filePath = PrepareSqlFile(db.Name, "dbo", "FTL", o.Name, currentPath, "");
-        //            Helpers.WriteSQLInner<FullTextStopList>(db.Name, "dbo", "FTL", o.Name, outputWriter, o, ScriptOption.Default, _logger);
-        //        }
-        //    }
-
-        //    /// *************************************************************
-        //    /// STORED PROCEDURES
-        //    //currentPath = csFile.CreateFolder(dbPath, pathify("PROCEDURE"));
-        //    foreach (StoredProcedure o in db.StoredProcedures.Cast<StoredProcedure>().AsQueryable().Where(o => o.IsSystemObject == false))
-        //    {
-        //        // filePath = PrepareSqlFile(db.Name, o.Schema, "PROCEDURE", o.Name, currentPath, "");
-        //        Helpers.WriteSQLInner<StoredProcedure>(db.Name, o.Schema, "PROCEDURE", o.Name, outputWriter, o, ScriptOption.Default, _logger);
-        //    }
-
-        //    /// *************************************************************
-        //    /// FUNCTIONS
-        //    // currentPath = csFile.CreateFolder(dbPath, pathify("FUNCTION"));
-        //    foreach (UserDefinedFunction o in db.UserDefinedFunctions.Cast<UserDefinedFunction>().Where(oo => oo.IsSystemObject == false))
-        //    {
-        //        Helpers.WriteSQLInner<UserDefinedFunction>(
-        //            db.Name, o.Schema, "FUNCTION", o.Name, outputWriter, o, ScriptOption.Default, _logger);
-        //    }
-
-        //    /// *************************************************************
-        //    /// TABLE
-        //    foreach (Table o in db.Tables.Cast<Table>().AsQueryable().Where(o => o.IsSystemObject == false))
-        //    {
-        //        _logger.LogInformation($"Export table - {o.Name}");
-
-        //        //currentPath = csFile.CreateFolder(dbPath, pathify("TABLE"));
-        //        //filePath = PrepareSqlFile(db.Name, o.Schema, "TABLE", o.Name, currentPath, "");
-        //        Helpers.WriteSQLInner<Table>(db.Name, o.Schema, "TABLE", o.Name, outputWriter, o, ScriptOption.Default, _logger);
-        //        if (_options.ScriptIndexes) 
-        //        { 
-        //            Helpers.WriteSQLInner<Table>(db.Name, o.Schema, "TABLE", o.Name, outputWriter, o, ScriptOption.Indexes, _logger);
-        //        }
-        //        Helpers.WriteSQLInner<Table>(db.Name, o.Schema, "TABLE", o.Name, outputWriter, o, ScriptOption.DriAll, _logger);
-
-        //        //////////////////////////////////////////////////////////////////////////
-        //        /// TABLE TRIGGERS
-        //        //currentPath = csFile.CreateFolder(dbPath, pathify("TRIGGER"));
-        //        foreach (Trigger ot in o.Triggers.Cast<Trigger>().AsQueryable().Where(oo => oo.IsSystemObject == false))
-        //        {
-        //            //filePath = PrepareSqlFile(db.Name, o.Schema, "TRIGGER", ot.Name, currentPath, "TABLE_" + o.Name);
-        //            Helpers.WriteSQLInner<Trigger>(db.Name, o.Schema, "TRIGGER", ot.Name, outputWriter, ot, ScriptOption.Default, _logger);
-        //        }
-
-        //        //////////////////////////////////////////////////////////////////////////
-        //        /// TABLE STATISTICS
-        //        if (_options.ExportStatistics)
-        //        {
-        //            //currentPath = csFile.CreateFolder(dbPath, pathify("STATISTIC"));
-        //            foreach (Statistic ot in o.Statistics.Cast<Statistic>().AsQueryable())
-        //            {
-        //                //filePath = PrepareSqlFile(db.Name, o.Schema, "STATISTIC", ot.Name, currentPath, "TABLE_" + o.Name);
-        //                Helpers.WriteSQLInner<Statistic>(db.Name, o.Schema, "STATISTIC", ot.Name, outputWriter, ot, ScriptOption.OptimizerData, _logger);
-        //            }
-        //        }
-        //    }
-
-        //    //////////////////////////////////////////////////////////////////////////
-        //    // VIEWS
-        //    if (_options.ExportViews)
-        //    {
-        //        foreach (View o in db.Views
-        //            .Cast<View>()
-        //            .AsQueryable()
-        //            .Where(o => o.IsSystemObject == false))
-        //        {
-
-        //            // currentPath = csFile.CreateFolder(dbPath, pathify("VIEW"));
-        //            // filePath = PrepareSqlFile(db.Name, o.Schema, "VIEW", o.Name, currentPath, "");
-        //            Helpers.WriteSQLInner<View>(db.Name, o.Schema, "VIEW", o.Name, outputWriter, o, ScriptOption.Default, _logger);
-        //            Helpers.WriteSQLInner<View>(db.Name, o.Schema, "VIEW", o.Name, outputWriter, o, ScriptOption.Indexes, _logger);
-        //            Helpers.WriteSQLInner<View>(db.Name, o.Schema, "VIEW", o.Name, outputWriter, o, ScriptOption.DriAllConstraints, _logger);
-
-        //            //////////////////////////////////////////////////////////////////////////
-        //            //VIEW TRIGGERS
-        //            //currentPath = csFile.CreateFolder(dbPath, pathify("TRIGGER"));
-        //            foreach (Trigger ot in o.Triggers.Cast<Trigger>().AsQueryable().Where(oo => oo.IsSystemObject == false))
-        //            {
-        //                //filePath = PrepareSqlFile(db.Name, o.Schema, "TRIGGER", ot.Name, currentPath, "VIEW_" + o.Name);
-        //                Helpers.WriteSQLInner<Trigger>(db.Name, o.Schema, "TRIGGER", ot.Name, outputWriter, ot, ScriptOption.Default, _logger);
-        //            }
-
-        //            //////////////////////////////////////////////////////////////////////////
-        //            //VIEW STATISTICS
-        //            if (_options.ExportStatistics)
-        //            {
-        //                //currentPath = csFile.CreateFolder(dbPath, pathify("STATISTIC"));
-        //                foreach (Statistic ot in o.Statistics.Cast<Statistic>().AsQueryable())
-        //                {
-        //                    //filePath = PrepareSqlFile(db.Name, o.Schema, "STATISTIC", ot.Name, currentPath, "VIEW_" + o.Name);
-        //                    Helpers.WriteSQLInner<Statistic>(db.Name, o.Schema, "STATISTIC", ot.Name, outputWriter, ot, ScriptOption.OptimizerData, _logger);
-        //                }
-        //            }
-        //        }
-        //    }
-        //}
-
-        //outputWriter.Flush();
-        //outputWriter.Close();
     }
 }
